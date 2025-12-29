de272e211cc251afbc938240ed79593d80752108
Humanoid-1 <uzaif@humanoidmaker.com>
2025-12-29 17:37:32 +0530

    add vercel domain

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const allowedOrigins = [
-  "https://web-frontend-pi-sable.vercel.app", // your deployed frontend
+  "web-frontend-oq2y.vercel.app", // your deployed frontend
   "http://localhost:5173"                     // local dev
 ];
 
